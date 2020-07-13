a03be031c33fa14a82be3335d1a73abf56667049
pvictorf <41065334+pvictorf@users.noreply.github.com>
2020-07-13 18:32:46 -0300

    site

---
+++ _cdn/_js/main.js
@@ -0,0 +1,9 @@
+function openMenu() {
+
+   let header_menu_sub = document.querySelector('.main_header_menu_mobile_sub');
+   let header_menu_mobile = document.querySelector('.main_header_menu_mobile_obj');
+
+   header_menu_sub.classList.toggle('ds_none');
+   header_menu_mobile.classList.toggle('main_header_menu_mobile_obj_active');
+
+}   
